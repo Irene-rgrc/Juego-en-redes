bbc93193c9c8686f82caafed70a140d4899b17c4
GhYB30 <ghaudy.gbe@gmail.com>
2024-01-20 19:22:24 +0100

    Revert "Intento de subir spring"
    This reverts commit 4fdc1804b6ba8ee81fda754692167817034453cf.

--- CaidaCelestial_ElDesafioDeLosExiliados/src/main/resources/templates/OnlineList.js
+++
@@ -1,8 +0,0 @@
-function loadItems(callback) {
-    $.ajax({
-        url: 'http://localhost:8080/items'
-    }).done(function (items) {
-        console.log('Items loaded: ' + JSON.parse(""));
-        callback(items);
-    })
-}
