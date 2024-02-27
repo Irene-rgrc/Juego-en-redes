1f7b968f029d77903d8b2515ca5d62437e9d34aa
GhYB30 <ghaudy.gbe@gmail.com>
2024-02-27 20:35:10 +0100

    Websocket no funciona

--- CaidaCelestialClienteServidor/src/main/java/com/caidacelestial/CaidaCelestialClienteServidorApplication.java
+++ CaidaCelestialClienteServidor/src/main/java/com/caidacelestial/CaidaCelestialClienteServidorApplication.java
@@ -2,12 +2,27 @@ package com.caidacelestial;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
 
 @SpringBootApplication
-public class CaidaCelestialClienteServidorApplication {
+@EnableWebSocket
+public class CaidaCelestialClienteServidorApplication implements WebSocketConfigurer{
+	@Override
+	public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+			registry.addHandler(handlerCC(), "/caidacelestial")
+			.setAllowedOrigins("*");
+	}
 
+	@Bean
+	public WebSocketCCHandler handlerCC() {
+		return new WebSocketCCHandler();
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CaidaCelestialClienteServidorApplication.class, args);
 	}
-
 }
---
+++ CaidaCelestialClienteServidor/src/main/java/com/caidacelestial/WebSocketCCHandler.java
@@ -0,0 +1,19 @@
+package com.caidacelestial;
+
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public class WebSocketCCHandler extends TextWebSocketHandler {
+
+	@Override
+	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+		//ObjectMapper mapper = new ObjectMapper();
+		//JsonNode node = mapper.readTree(message.getPayload());
+		System.out.println("Message received: " + message);
+	}
+
+}
