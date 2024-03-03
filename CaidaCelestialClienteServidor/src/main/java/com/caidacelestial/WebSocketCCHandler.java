b577d96fbb482244343b075f4517550cd7122cbb
GhYB30 <ghaudy.gbe@gmail.com>
2024-03-03 18:13:55 +0100

    intento de inicio de sesion

--- CaidaCelestialClienteServidor/src/main/java/com/caidacelestial/WebSocketCCHandler.java
+++ CaidaCelestialClienteServidor/src/main/java/com/caidacelestial/WebSocketCCHandler.java
@@ -8,12 +8,28 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class WebSocketCCHandler extends TextWebSocketHandler {
-
+	boolean primero=false;
+	
 	@Override
 	protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
 		ObjectMapper mapper = new ObjectMapper();
 		JsonNode node = mapper.readTree(message.getPayload());
+		JsonNode m = node.get("peticion");
 		System.out.println("Message received: " + node.toString());
+		
+		switch(m.toString()){
+		case "emparejar":
+			if(!primero) {
+				primero=true;
+				String msg="Cassadie";
+				session.sendMessage(new TextMessage(msg));
+			}
+			else {
+				String msg="Seraphina";
+				session.sendMessage(new TextMessage(msg));
+			}
+			break;
+		}
 	}
 
 }
